b74209badd4db0ba480f1969761f3b2a1368c372
sdegen95 <147383941+sdegen95@users.noreply.github.com>
2024-04-02 16:13:41 +0200

    Add files via upload

--- Gal-conjecture/Updated/gal_from_nb.py
+++ Gal-conjecture/Updated/gal_from_nb.py
@@ -1,4 +1,35 @@
 
+# # **Counterexample for Gal's conjecture**
+# ***
+# ***
+
+# ## Description
+
+# **<ins>Gal's conjecture:</ins>** Let $\Delta$ be a simplicial complex. If $\Delta$ is flag simplicial (homology) sphere, then $\gamma(\Delta)$ is nonnegative.
+# 
+# **<ins>Notions:</ins>**
+# 
+# - A simplicial complex $\Delta$ called *flag*, if all minimal non-faces of $\Delta$ are of size $2$. 
+# - A simplicial complex $\Delta$ called *simplicial sphere*, if $\Delta$ triangulates a ($\dim(\Delta)$)-sphere.
+# - More generally a simplicial complex $\Delta$ called *homology sphere*, if for every face $F\in\Delta$, $\text{lk}_\Delta(F)$ has the same homology as a ($\dim(\text{lk}_\Delta(F)$)-sphere.
+# 
+# **<ins>Known results:</ins>** 
+# 
+# - In order to be a simplicial (homology) sphere, the $h$-vector has to be symmetric. 
+# - The conjecture is proven for dimension $3$ and $4$.
+# 
+# **<ins>Goal:</ins>** At the moment we try to find a counterexample for the homology sphere case in dimension $5$.
+# 
+# **<ins>Approach:</ins>**
+# 
+# - Consider a graph and construct it's corresponding clique complex.
+# - Compute: $f$-vector ------> $h$-vector ------> $\gamma$-vector.
+# - First: teaching the machine to only generate $h$-vectors of length $7$.
+# - Second: teaching the machine to only consider those $h$-vectors from above which are symmetric.
+# - Third: from the above two conditions of the $h$-vector, try teaching the machine to find a $\gamma$-vector with negative entries.
+
+# ## Inputs
+
 import networkx as nx
 import random
 import numpy as np
@@ -12,9 +43,19 @@ from torch import nn
 from cached_method import cached_method
 import time
 
-N = 17  # Number of vertices in the graph.
+# ## Fixed Values
+
+N = 20  # Number of vertices in the graph.
 M = N*(N-1)//2  # Number of edges in the graph.
 punish_value = 100000000
+Desired_DIM = 5
+RIGHT_EULER_CHAR = 0
+if Desired_DIM % 2 == 0:
+    RIGHT_EULER_CHAR = 2
+else:
+    RIGHT_EULER_CHAR = 0
+
+# ## The model
 
 class MYModel(nn.Module):
     def __init__(self, layer_dims):
@@ -43,6 +84,8 @@ class MYModel(nn.Module):
         x = torch.sigmoid(x)
         return x
 
+# ## Helper functions
+
 def board_to_adj(board):
     """
     Input: A tensor of shape (*, M), giving a (batched) 01-sequence of edges.
@@ -65,6 +108,24 @@ def display_adj(adj):
     nx.draw_circular(nx.complement(G))
     plt.savefig("mygraph.png")
 
+# display_adj(np.array([[0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 1, 0, 0, 1, 0],
+#  [0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+#  [0, 0, 0, 0, 0, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 1],
+#  [1, 1, 0, 0, 0, 0, 1, 0, 1, 0, 0, 0, 0, 1, 1, 1],
+#  [0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1, 1],
+#  [1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+#  [0, 1, 1, 1, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0],
+#  [0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+#  [1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+#  [1, 1, 0, 0, 1, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1],
+#  [0, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1, 1, 1, 1],
+#  [1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1],
+#  [0, 1, 0, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1],
+#  [0, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1],
+#  [1, 1, 0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 0, 1],
+#  [0, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0]]))
+
+# ### Compute $f$-vector, $h$-vector, $\gamma$-vector and $g$-vector
 
 def f_vector(adj):
     G = nx.convert_matrix.from_numpy_array(adj)
@@ -156,16 +217,47 @@ def link(cliques,face):
     
     return link_li
 
-# start = time.time()
+def g_vector(h_vec):
+    d = (len(h_vec)-1)//2
+    g_vec = [h_vec[i]-h_vec[i-1] for i in range(1,d+1)]
+    g_vec.insert(0,1)
+
+    return g_vec
+
+# adj = np.array([[0,1,1,1],
+#                 [1,0,1,1],
+#                 [1,1,0,1],
+#                 [1,1,1,0]])
+# f_vec, cliques = f_vector(adj)
+# h_vec = h_vector(f_vec)
+# g_vec = g_vector(h_vec)
+# gam_vec = gamma_vector(h_vec)
+# f_vec,h_vec,g_vec,gam_vec
+
+# ### Compute Euler-characteristic and check M-sequence condition
+
+def euler_char(f_vec):
+    e_char = 0
+    new_f_vec = [f_vec[i] for i in range(len(f_vec)) if i != 0]
+
+    # Sum up the entry of the new_f_vec in an alternating manner
+    for i in range(len(new_f_vec)):
+        if i % 2 == 0:
+            e_char += new_f_vec[i]
+        else:
+            e_char -= new_f_vec[i]
+    
+    return e_char
+
 # adj = np.array([[0,1,1,0],
 #                 [1,0,0,1],
 #                 [1,0,0,1],
 #                 [0,1,1,0]])
 # f_vec, cliques = f_vector(adj)
-# s = link(cliques,[1])
-# hvec = h_vector_from_simplices(s)
-# end = time.time()
-# print(end-start,hvec,s)
+# e_char = euler_char(f_vec)
+# f_vec,e_char
+
+# ### Games and learning
 
 def play_games(agent, k_games):
     """
@@ -224,19 +316,23 @@ def reinforce_games(
         loss.backward()
         optimiser.step()
 
+# ## Score function and Hyperparameter
 
 # Number of new games to play each round.
 K_GAMES = 500
 
 # Create a fresh network, and specify its architecture.
-model = MYModel([50, 40, 30])                                                                ## originally: [32, 12, 4]
+model = MYModel([100, 90, 70])                                                                ## originally: [32, 12, 4]
 
 # The learning rate should be increased if convergence is too slow, but decreased if
 # the algorithm keeps getting stuck in local minima.
-optimiser = torch.optim.Adam(model.parameters(), lr=0.001)                                  ## originally: torch.optim.Adam, lr=0.0009
+optimiser = torch.optim.SGD(model.parameters(), lr=0.1)                                  ## originally: torch.optim.Adam, lr=0.0009
 
 # The percentage of K_GAMES to reinforce.
 KEEP_TOP = int(round(K_GAMES * 0.1))
+
+# Estimation lower bound for a good negativity of gam_min
+LOWER_BOUND = -2
 
 # The scoring function to use
 def score_fn(adj):
@@ -248,52 +344,65 @@ def score_fn(adj):
         return punish_value
     else:
         G = nx.convert_matrix.from_numpy_array(adj)
-        if nx.node_connectivity(G) == 0:
-            return int(punish_value/2)
-        if nx.node_connectivity(G) == 1:
-            return int(punish_value/4)
-        if nx.node_connectivity(G) == 2:
-            return int(punish_value/5)
+        # if nx.node_connectivity(G) == 0:
+        #     return int(punish_value/2)
+        # if nx.node_connectivity(G) == 1:
+        #     return int(punish_value/4)
+        # if nx.node_connectivity(G) == 2:
+        #     return int(punish_value/5)
+
+        if nx.node_connectivity(G) != Desired_DIM + 1:
+            return punish_value
         
-        f_vec, cliques = f_vector(adj)
-
-        if len(f_vec) > 9:
-            return  int(punish_value/5)
         else:
-            h_vec = h_vector(f_vec)
-            
-            d = len(h_vec)-1
-            neg_components = abs(sum([h for h in h_vec if h < 0]))
+            f_vec, cliques = f_vector(adj)
 
-            if neg_components > 0:
-                return neg_components*10000
-
-            if d+1 == 7:
-                t = sum([abs(h_vec[d-i]-h_vec[i]) for i in range(d//2+1)])
-                if t > 0:
-                    return 100*t
-                elif t == 0:
-                    gam_vec = gamma_vector(h_vec)
-                    gam_min = min(gam_vec)
-                    #return gam_min
-                
-                    if gam_min < 0:
-                        considered_cliques = [x for x in cliques if len(x)==d-1 or len(x)==d-2]
-                        for face in considered_cliques:
-                            l = link(cliques,face)
-                            l_h_vec = h_vector_from_simplices(l)
-                            l_d = len(l_h_vec)-1
-                            l_t = sum([abs(l_h_vec[l_d-i]-l_h_vec[i]) for i in range(l_d//2+1)])
-                            if l_t > 0:
-                                gam_min += l_t
-                            elif l_t == 0:
-                                continue
-                        return gam_min 
-                    else:
-                        return gam_min + 100
+            if len(f_vec) > Desired_DIM+2:
+                return  int(punish_value/5)
             else:
-                return neg_components + 10000
+                if euler_char(f_vec) != RIGHT_EULER_CHAR:
+                    return int(punish_value/5)
+                else:
+                    h_vec = h_vector(f_vec)
+                    
+                    d = len(h_vec)-1
+                    neg_components = abs(sum([h for h in h_vec if h < 0]))
 
+                    if neg_components > 0:
+                        return neg_components*10000
+                    else:
+                        if d+1 == Desired_DIM+2:
+                            t = sum([abs(h_vec[d-i]-h_vec[i]) for i in range(d//2+1)])
+                            if t > 0:
+                                return 100*t
+                            elif t == 0:
+                                gam_vec = gamma_vector(h_vec)
+                                gam_min = min(gam_vec)
+                                #return gam_min
+                            
+                                if gam_min < 0:
+                                    if gam_min >= LOWER_BOUND: 
+                                        considered_cliques = [x for x in cliques if len(x)==d-1 or len(x)==d-2]
+                                        for face in considered_cliques:
+                                            l = link(cliques,face)
+                                            l_h_vec = h_vector_from_simplices(l)
+                                            l_d = len(l_h_vec)-1
+                                            l_t = sum([abs(l_h_vec[l_d-i]-l_h_vec[i]) for i in range(l_d//2+1)])
+                                            if l_t > 0:
+                                                gam_min += l_t*0.01
+                                            elif l_t == 0:
+                                                continue
+                                        return gam_min
+                                    else:
+                                        return abs(gam_min)
+                                else:
+                                    return gam_min + 100
+                        else:
+                            return neg_components + 10000
+
+# ## Run the process
+
+# This cell can be re-run without resetting the model. 
 top_games = np.ones((0, M), dtype=np.float32)
 
 collection = []
@@ -343,55 +452,49 @@ def run():
 
     print(board_to_adj(top_games[0]))
 
+run()
 
-#run()
-
-
-
+# ## Visualize the results
 
 # new_collec = np.unique(collection,axis=0)
 # new_collec[1]
 
-
 # adj = new_collec[1]
 # h_vector(f_vector(adj)[0]), gamma_vector(h_vector(f_vector(adj)[0]))
 
-
 # display_adj(adj)
-
 
 # g = nx.convert_matrix.from_numpy_array(adj)
 # #[x for x in list(nx.enumerate_all_cliques(g)) if len(x)>=6]
 # max(list(nx.find_cliques(g)))
 
+# ## Checking homology sphere condition
 
+def clique_condition(adj,dim): # dim is simplicial-complex dimension
+    holds = True
 
+    # Create graph and compute the necessary cliques
+    g = nx.convert_matrix.from_numpy_array(adj)
+    cliques = list(nx.enumerate_all_cliques(g))
+    biggest_cliques = [x for x in cliques if len(x)==dim+1]
+    second_biggest_cliques = [x for x in cliques if len(x)==dim]
 
-# def clique_condition(adj,dim): # dim is simplicial-complex dimension
-#     holds = True
+    # Checking the condition
+    for elm1 in second_biggest_cliques:
+        count = 0
+        for elm2 in biggest_cliques:
+            if set(elm1).issubset(set(elm2)):
+                count +=1
+            else:
+                continue
+        if count == 2:
+            #print("I'm here")
+            continue
+        else:
+            holds = False
+            break
 
-#     # Create graph and compute the necessary cliques
-#     g = nx.convert_matrix.from_numpy_array(adj)
-#     cliques = list(nx.enumerate_all_cliques(g))
-#     biggest_cliques = [x for x in cliques if len(x)==dim+1]
-#     second_biggest_cliques = [x for x in cliques if len(x)==dim]
-
-#     # Checking the condition
-#     for elm1 in second_biggest_cliques:
-#         count = 0
-#         for elm2 in biggest_cliques:
-#             if set(elm1).issubset(set(elm2)):
-#                 count +=1
-#             else:
-#                 continue
-#         if count == 2:
-#             #print("I'm here")
-#             continue
-#         else:
-#             holds = False
-#             break
-
-#     return holds
+    return holds
 
 # adj = new_collec[0]
 
